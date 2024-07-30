87231be045eaafac12be31ee4a47e890b04b4ad9
flomero <49640251+flomero@users.noreply.github.com>
2024-07-30 17:58:06 +0200

    remove endl

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 21:28:35 by flfische          #+#    #+#             */
-/*   Updated: 2024/07/29 21:35:21 by flfische         ###   ########.fr       */
+/*   Updated: 2024/07/30 17:57:15 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int main(void)
 	std::string str = "HI THIS IS BRAIN";
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
-	std::cout << "Adresses:" << std::endl;
-	std::cout << "\t- String:\t" << &str << std::endl;
-	std::cout << "\t- StringPTR:\t" << stringPTR << std::endl;
-	std::cout << "\t- StringREF:\t" << &stringREF << std::endl;
-	std::cout << "Values:" << std::endl;
-	std::cout << "\t- String:\t" << str << std::endl;
-	std::cout << "\t- StringPTR:\t" << *stringPTR << std::endl;
-	std::cout << "\t- StringREF:\t" << stringREF << std::endl;
+	std::cout << "Adresses:\n"
+			  << "	- String:	" << &str << "\n"
+			  << "	- StringPTR:	" << stringPTR << "\n"
+			  << "	- StringREF:	" << &stringREF << "\n"
+			  << "Values:\n"
+			  << "\t- String:\t" << str << "\n"
+			  << "\t- StringPTR:\t" << *stringPTR << "\n"
+			  << "\t- StringREF:\t" << stringREF << std::endl;
 	return (0);
 }
