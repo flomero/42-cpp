b22b5cc35deceab1c5cbd189fbff93bdf2562fd5
flomero <49640251+flomero@users.noreply.github.com>
2024-07-31 12:33:40 +0200

    fix megaphone:

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 11:17:12 by flfische          #+#    #+#             */
-/*   Updated: 2024/07/30 15:25:35 by flfische         ###   ########.fr       */
+/*   Updated: 2024/07/31 12:31:46 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,18 @@
 int main(int argc, char **argv)
 {
 	std::string str;
-	int i;
 
 	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	i = 1;
-	while (i < argc)
+	for (int i = 1; i < argc; i++)
 	{
 		str = argv[i];
 		for (size_t j = 0; j < str.length(); j++)
-			std::cout << static_cast<char>(toupper(str[j]));
-		i++;
+			str[j] = std::toupper(str[j]);
+		std::cout << str;
 	}
 	std::cout << std::endl;
 }
