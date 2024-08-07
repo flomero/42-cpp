51a2d8ec8abd084a4382b80f9f50701a3ff3a3c3
flomero <49640251+flomero@users.noreply.github.com>
2024-08-07 17:54:03 +0200

    cpp03 ex00 wip

---
+++ CPP03/ex00/Claptrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Claptrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 17:38:42 by flfische          #+#    #+#             */
+/*   Updated: 2024/08/07 17:41:45 by flfische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	ClapTrap();
+	std::string _name;
+	unsigned int _hitpoints = 10;
+	unsigned int _energyPoints = 10;
+	unsigned int _attackDamage = 0;
+
+public:
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &other);
+	ClapTrap &operator=(const ClapTrap &other);
+	~ClapTrap();
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
