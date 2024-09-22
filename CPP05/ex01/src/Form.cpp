6939df63deec7a13cbaf42bac0c8a13bd87efaae
flomero <49640251+flomero@users.noreply.github.com>
2024-09-22 10:42:34 +0200

    ex01 wip

---
+++ CPP05/ex01/src/Form.cpp
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 19:18:18 by flfische          #+#    #+#             */
+/*   Updated: 2024/09/22 10:41:52 by flfische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form(std::string const name, int gradeToSign, int gradeToExecute)
+	: _name(name), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute)
+{
+	if (gradeToSign < 1 || gradeToExecute < 1)
+		throw Form::GradeTooHighException();
+	if (gradeToSign > 150 || gradeToExecute > 150)
+		throw Form::GradeTooLowException();
+	_signed = false;
+}
+
+Form::Form(Form const &other) : _name(other._name), _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute)
+{
+	_signed = other._signed;
+}
+
+Form &Form::operator=(Form const &other)
+{
+	if (this == &other)
+		return *this;
+	_signed = other._signed;
+	return *this;
+}
+
+bool Form::getSigned() const
+{
+	return _signed;
+}
+
+int Form::getGradeToSign() const
+{
+	return _gradeToSign;
+}
+
+int Form::getGradeToExecute() const
+{
+	return _gradeToExecute;
+}
+
+std::string const &Form::getName() const
+{
+	return _name;
+}
+
+void Form::beSigned(Bureaucrat const &bureaucrat)
+{
+	if (bureaucrat.getGrade() > _gradeToSign)
+		throw Form::GradeTooLowException();
+	_signed = true;
+}
+
+const char *Form::GradeTooHighException::what() const throw()
+{
+	return "Grade is too high";
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return "Grade is too low";
+}
+
+std::ostream &operator<<(std::ostream &out, Form const &form)
+{
+	out << "----- " << form.getName() << " -----" << std::endl;
+	out << "Signed: " << form.getSigned() << std::endl;
+	out << "Grade to sign: " << form.getGradeToSign() << std::endl;
+	out << "Grade to execute: " << form.getGradeToExecute() << std::endl;
+	return out;
+}
--- CPP05/ex01/src/main.cpp
+++ CPP05/ex01/src/main.cpp
@@ -6,28 +6,26 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 22:56:08 by flfische          #+#    #+#             */
-/*   Updated: 2024/09/21 14:13:02 by flfische         ###   ########.fr       */
+/*   Updated: 2024/09/22 10:40:44 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main()
 {
 	try
 	{
-		std::cout << "-------------------" << std::endl;
-		Bureaucrat bureaucrat("Bureaucrat", 1);
-		bureaucrat.decrementGrade();
+		Bureaucrat bureaucrat("Bureaucrat", 150);
 		std::cout << bureaucrat << std::endl;
-		bureaucrat.incrementGrade();
-		std::cout << bureaucrat << std::endl;
-		bureaucrat.incrementGrade();
-		std::cout << bureaucrat << std::endl;
+		Form form("Form", 150, 150);
+		std::cout << form << std::endl;
+		form.beSigned(bureaucrat);
+		std::cout << form << std::endl;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << "-------------------" << std::endl;
 		std::cerr << e.what() << std::endl;
 	}
 }
