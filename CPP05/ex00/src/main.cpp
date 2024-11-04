809afe605428b30223c305840561ef29bcc52d1a
flomero <49640251+flomero@users.noreply.github.com>
2024-11-04 16:04:08 +0100

    add more tests

--- CPP05/ex00/src/main.cpp
+++ CPP05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 22:56:08 by flfische          #+#    #+#             */
-/*   Updated: 2024/09/21 14:13:02 by flfische         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:02:04 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,26 @@ int main()
 	{
 		std::cout << "-------------------" << std::endl;
 		Bureaucrat bureaucrat("Bureaucrat", 1);
+		std::cout << bureaucrat << std::endl;
 		bureaucrat.decrementGrade();
 		std::cout << bureaucrat << std::endl;
 		bureaucrat.incrementGrade();
 		std::cout << bureaucrat << std::endl;
 		bureaucrat.incrementGrade();
+		std::cout << bureaucrat << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "-------------------" << std::endl;
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << "-------------------" << std::endl;
+		Bureaucrat bureaucrat("Bureaucrat", 150);
+		std::cout << bureaucrat << std::endl;
+		bureaucrat.decrementGrade();
 		std::cout << bureaucrat << std::endl;
 	}
 	catch (std::exception &e)
