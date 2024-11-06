618669559fb971c62a62334114537ccd824a9130
flomero <49640251+flomero@users.noreply.github.com>
2024-11-06 10:49:29 +0100

    more tests

--- CPP05/ex03/src/main.cpp
+++ CPP05/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 22:56:08 by flfische          #+#    #+#             */
-/*   Updated: 2024/11/05 20:21:09 by flfische         ###   ########.fr       */
+/*   Updated: 2024/11/06 10:48:49 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,29 @@ int main()
 		bureaucrat.executeForm(*rrf);
 
 		delete rrf;
+	}
+	std::cout << "_________________________" << std::endl;
+	{
+		Intern someRandomIntern;
+		AForm* ppf;
+		ppf = someRandomIntern.makeForm("presidential pardon", "Bender");
+
+		Bureaucrat bureaucrat("Bureaucrat", 1);
+		bureaucrat.signForm(*ppf);
+		bureaucrat.executeForm(*ppf);
+
+		delete ppf;
+	}
+	std::cout << "_________________________" << std::endl;
+	{
+		Intern someRandomIntern;
+		AForm* scf;
+		scf = someRandomIntern.makeForm("shrubbery creation", "Bender");
+
+		Bureaucrat bureaucrat("Bureaucrat", 1);
+		bureaucrat.signForm(*scf);
+		bureaucrat.executeForm(*scf);
+
+		delete scf;
 	}
 }
