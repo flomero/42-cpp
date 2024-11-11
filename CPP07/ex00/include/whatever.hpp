765a31ea916db5898025e6570be608ed4cfccf26
flomero <49640251+flomero@users.noreply.github.com>
2024-11-11 21:36:05 +0100

    cpp07 ex00

---
+++ CPP07/ex00/include/whatever.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 20:36:20 by flfische          #+#    #+#             */
+/*   Updated: 2024/11/11 20:37:36 by flfische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b ? a : b);
+}
