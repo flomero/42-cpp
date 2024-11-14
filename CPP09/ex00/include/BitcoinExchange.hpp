4695dd55ce84231a274e2d1cc5a9a2e633d2cdd4
flomero <49640251+flomero@users.noreply.github.com>
2024-11-14 12:48:09 +0100

    start cpp09

---
+++ CPP09/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 12:45:25 by flfische          #+#    #+#             */
+/*   Updated: 2024/11/14 12:47:35 by flfische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class BitcoinExchange
+{
+	private:
+		/* data */
+	public:
+		BitcoinExchange() = default;
+		~BitcoinExchange() = default;
+};
---
+++ CPP09/ex00/src/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 12:46:13 by flfische          #+#    #+#             */
+/*   Updated: 2024/11/14 12:47:49 by flfische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main()
+{
+	BitcoinExchange bitcoinExchange;
+	(void)bitcoinExchange;
+	return 0;
+}
